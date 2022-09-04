6fa79ec3f5718f7ed630dfe36da45bf7e2d6844d
Aleš Kučera <kuceral4@fel.cvut.cz>
2022-09-04 22:50:46 +0200

    Arguments for rci

--- src/train.py
+++ src/train.py
@@ -47,15 +47,15 @@ def create_model(architecture, n_inputs, n_outputs, pretrained=True):
 parser = ArgumentParser()
 parser.add_argument('--lr', type=float, default=1e-5)
 parser.add_argument('--architecture', type=str, default='fcn_resnet50')
-parser.add_argument('--batch_size', type=int, default=16)
+parser.add_argument('--batch_size', type=int, default=8)
 parser.add_argument('--img_size', nargs='+', default=(320, 512))
 parser.add_argument('--n_epochs', type=int, default=20)
 parser.add_argument('--n_workers', type=int, default=os.cpu_count())
 args = parser.parse_args()
 args.img_size = tuple(args.img_size)
 
-train_dataset = OFFSEG(crop_size=args.img_size, split='train')
-valid_dataset = OFFSEG(crop_size=args.img_size, split='val')
+train_dataset = OFFSEG(crop_size=args.img_size, split='train', size=5000)
+valid_dataset = OFFSEG(crop_size=args.img_size, split='val', size=5000)
 
 train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.n_workers // 2)
 valid_loader = DataLoader(valid_dataset, batch_size=1, shuffle=False, num_workers=args.n_workers // 2)
